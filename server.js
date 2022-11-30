431a3d668378c44533535bfc1029ec7c06beba01
RulexNODE <118261010+RulexNODE@users.noreply.github.com>
2022-11-30 11:10:28 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
-// Listen on a specific host via the HOST environment variable
-var host = process.env.HOST || "localhost";
-// Listen on a specific port via the PORT environment variable
-var port = process.env.PORT || 8080;
+// cors-anywhere deployed as my own server due to the public demo URL
+// restricted to a certain number of uses
 
-var cors_proxy = require("cors-anywhere");
-cors_proxy
-  .createServer({
-  checkRateLimit: checkRateLimit,
+// Listen on a specific host via the HOST environment variable
+var host = process.env.HOST || '0.0.0.0';
+// Listen on a specific port via the PORT environment variable
+var port = process.env.PORT || '8080';
+
+var cors_proxy = require('cors-anywhere');
+cors_proxy.createServer({
   removeHeaders: ['cookie', 'cookie2'],
   redirectSameOrigin: true,
   httpProxyOptions: {xfwd: false},
-  })
-  .listen(port, host, function () {
-    console.log("Running CORS Anywhere on " + host + ":" + port);
-  });
+}).listen(port, host, function() {
+    console.log('Running CORS Anywhere on ' + host + ':' + port);
+});
